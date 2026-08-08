9ad8f07930c4ec7b28e266c0247ee13c257d1b80
shruti <shrutipandey1107006>
2026-08-08 21:59:05 +0530

    Add Day01 Python solutions

--- LNCEBTC31006_SHRUTIPANDEY/Day01/problem_643.py
+++ LNCEBTC31006_SHRUTIPANDEY/Day01/problem_643.py
@@ -7,7 +7,6 @@ class Solution:
         for i in range(k, len(nums)):
             window_sum += nums[i]
             window_sum -= nums[i - k]
-
             max_sum = max(max_sum, window_sum)
 
         return max_sum / k
