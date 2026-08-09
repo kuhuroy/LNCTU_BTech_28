0bad854fb51becd9d5e6446544f9c3204cd6b5e9
shruti <shrutipandey1107006>
2026-08-09 16:36:24 +0530

    Completed Day 1 tasks

--- LNCEBTC31006_SHRUTIPANDEY/Day01/problem_303.py
+++ LNCEBTC31006_SHRUTIPANDEY/Day01/problem_303.py
@@ -1,10 +1,21 @@
 class NumArray:
 
     def __init__(self, nums):
+        # Store prefix sums to answer range sum quickly
         self.prefix = [0]
 
         for num in nums:
             self.prefix.append(self.prefix[-1] + num)
 
     def sumRange(self, left, right):
+        # Sum from left to right using prefix sum
         return self.prefix[right + 1] - self.prefix[left]
+
+
+# Example
+nums = [-2, 0, 3, -5, 2, -1]
+obj = NumArray(nums)
+
+print(obj.sumRange(0, 2))
+print(obj.sumRange(2, 5))
+print(obj.sumRange(0, 5))
